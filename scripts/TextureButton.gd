6dc0ff92c6f3423a413e375d8f268a49cb2d920b
Caleb Key <caleb.key@us.af.mil>
2024-05-12 19:20:17 -0500

    init

---
+++ scripts/TextureButton.gd
@@ -0,0 +1,14 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.pressed.connect(self._on_Button_pressed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_Button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
